360fb8ee77d69cf81b0a98dc2f71dd08b95e8458
Lubaba Nawla <lubabanawla@gmail.com>
2024-09-27 11:28:39 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        info = game.getScore();
+        System.out.println(info);
+
+        info = game.getScore();
+        System.out.println(info);
+
+    }
+}
